25133be0f12bc2deeb9bde90836197cad9f7ade4
Oliver Thomas <1835492+olliethomas@users.noreply.github.com>
2020-06-01 21:27:29 +0100

    hpc scripts

---
+++ scripts/cmnist_2_digits.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#Expects cwd to be fair-dist-matching. i.e ./scripts/cmnist_1_digits.sh
+seeds=( 888 1 2410 1996 511 )
+scales=( 0.0 0.005 0.01 0.015 0.02 0.025 0.03 0.035 0.04 0.045 0.05 )
+
+slots=1
+
+for scale in "${scales[@]}"; do
+    echo $scale
+    for seed in "${seeds[@]}"; do
+        echo $seed
+        qsub -pe smpslots $slots python-ot.job run_both.py @flags/cmnist_ae_2_digits.yaml \
+        --b-super-val-freq 20 \
+        --b-super-val True \
+        --b-num-disc-updates 3 \
+        --b-disc-hidden 256 256 \
+        --b-warmup-steps 1000 \
+        --b-seed $seed \
+        --b-scale $scale \
+        --d-results cmnist/2_digits/$seed/$scale.csv \
+        --b-save-dir experiments/cmnist/$seed $@
+        sleep 1
+    done
+done
