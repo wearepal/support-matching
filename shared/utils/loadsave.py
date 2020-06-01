a84bab22147e98aa28e2dd6eb2866977c16388d9
Thomas M <thomas5max@gmail.com>
2020-06-01 12:54:02 +0100

    Run the parts of the code in subprocesses

--- shared/utils/loadsave.py
+++ shared/utils/loadsave.py
@@ -10,19 +10,24 @@ __all__ = ["save_results", "load_results"]
 
 def save_results(args: BaseArgs, cluster_ids: torch.Tensor, results_dir: Path) -> Path:
     """Save a tensor in a file."""
-    save_path = results_dir / "cluster_results.pth"
+    if args.cluster_label_file:
+        save_path = Path(args.cluster_label_file)
+    else:
+        save_path = results_dir / "cluster_results.pth"
     save_dict = {
         "args": args.as_dict(),
         "cluster_ids": cluster_ids,
     }
     torch.save(save_dict, save_path)
-    print(f"Saved results in {save_path.resolve()}")
+    print(
+        f"To make use of the generated cluster labels:\n--cluster-label-file {save_path.resolve()}"
+    )
     return save_path
 
 
-def load_results(args: BaseArgs, cluster_label_file: Path) -> torch.Tensor:
+def load_results(args: BaseArgs) -> torch.Tensor:
     """Load a tensor from a file."""
-    data = torch.load(cluster_label_file, map_location=torch.device("cpu"))
+    data = torch.load(args.cluster_label_file, map_location=torch.device("cpu"))
     saved_args = data["args"]
     assert saved_args["dataset"] == args.dataset, f'{saved_args["dataset"]} != {args.dataset}'
     assert (
@@ -37,5 +42,4 @@ def load_results(args: BaseArgs, cluster_label_file: Path) -> torch.Tensor:
     assert (
         saved_args["test_pcnt"] == args.test_pcnt
     ), f'{saved_args["test_pcnt"]} != {args.test_pcnt}'
-    args.cluster_label_file = str(cluster_label_file)
     return data["cluster_ids"]
