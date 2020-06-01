ed2c9f43313dcd3067b53c9bc41d1acc1e540759
Thomas M <thomas5max@gmail.com>
2020-06-01 12:54:02 +0100

    Run the parts of the code in subprocesses

--- fdm/optimisation/train.py
+++ fdm/optimisation/train.py
@@ -56,7 +56,7 @@ Generator = Union[AutoEncoder, PartitionedAeInn]
 
 def main(
     raw_args: Optional[List[str]] = None,
-    known_only: bool = False,
+    known_only: bool = True,  # TODO: find a way for known_only=False to work
     cluster_label_file: Optional[Path] = None,
 ) -> Generator:
     """Main function
@@ -107,7 +107,7 @@ def main(
     dataloader_args: Dict[str, Any]
 
     if ARGS.cluster_label_file:
-        cluster_ids = load_results(ARGS, Path(ARGS.cluster_label_file))
+        cluster_ids = load_results(ARGS)
         weights, n_clusters, min_count = weight_for_balance(cluster_ids)
         # we subsample the larger clusters rather than supersample the smaller clusters
         sample_with_replacement = False
--- run_both.py
+++ run_both.py
@@ -1,12 +1,14 @@
 """Call the main functions of both parts one after the other."""
+from pathlib import Path
+from subprocess import run
 import sys
+from tempfile import TemporaryDirectory
 
 assert sys.version_info >= (3, 8), f"please use Python 3.8 (this is 3.{sys.version_info.minor})"
-from fdm.optimisation import main as fdm
-from clustering.optimisation import main as clustering
 
-if __name__ == "__main__":
-    # first run the clustering, then pass on the cluster labels to the fair representation code
+
+def main():
+    """First run the clustering, then pass on the cluster labels to the fair representation code."""
     raw_args = sys.argv[1:]
     if not all(
         (not arg.startswith("--")) or arg.startswith(("--c-", "--d-", "--b-")) for arg in raw_args
@@ -18,12 +20,14 @@ if __name__ == "__main__":
             "So, for example: --b-dataset cmnist --c-epochs 100"
         )
         raise RuntimeError("all flags have to use the prefix '--b-', '--c-' or '--d-'.")
-    result = clustering(
-        [arg.replace("--c-", "--").replace("--b-", "--") for arg in raw_args], known_only=True
-    )
-    if result is not None:
-        fdm(
-            [arg.replace("--d-", "--").replace("--b-", "--") for arg in raw_args],
-            known_only=True,
-            cluster_label_file=result[1],
-        )
+    with TemporaryDirectory() as tmpdir:
+        clf = str(Path(tmpdir) / "labels.pth")
+        clf_flag = ["--cluster-label-file", clf]
+        clust_args = [arg.replace("--c-", "--").replace("--b-", "--") for arg in raw_args]
+        run([sys.executable, "run_cl.py"] + clust_args + clf_flag, check=True)
+        dis_args = [arg.replace("--d-", "--").replace("--b-", "--") for arg in raw_args]
+        run([sys.executable, "run.py"] + dis_args + clf_flag, check=True)
+
+
+if __name__ == "__main__":
+    main()
