fc9ba256d6ef88c206ad6e8b7e40b9b9117ed95b
Myles Bartlett <44702700+MylesBartlett@users.noreply.github.com>
2020-06-02 22:58:44 +0100

    Log weighted losses.

--- clustering/models/methods.py
+++ clustering/models/methods.py
@@ -43,15 +43,15 @@ class Method:
             logits = classifier(z)
 
             if ce_weight:
-                ce_loss = classifier.apply_criterion(logits=logits, targets=class_id).mean()
-                loss += ce_loss * ce_weight
+                ce_loss = ce_weight * classifier.apply_criterion(logits=logits, targets=class_id).mean()
+                loss += ce_loss
                 logging_dict["Loss supervised (CE)"] = ce_loss.item()
             if bce_weight:
                 preds = F.softmax(logits, dim=-1)
                 label = (class_id.unsqueeze(1) == class_id).float()
                 mask = torch.ones_like(label)
-                bce_loss = _cosine_and_bce(preds, label, mask)
-                loss += bce_weight * bce_loss
+                bce_loss = bce_weight * _cosine_and_bce(preds, label, mask)
+                loss += bce_loss
                 logging_dict["Loss supervised (BCE)"] = bce_loss.item()
 
         return loss, logging_dict
