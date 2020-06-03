0f9ec3c8f4a6a2773dbd4009146ac304904bcad5
Oliver Thomas <1835492+olliethomas@users.noreply.github.com>
2020-06-03 20:14:03 +0100

    add baseline 2 script

---
+++ scripts/hpc/hpc_cmnist_kmeans_2_digits.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#Expects cwd to be fair-dist-matching. i.e ./scripts/cmnist_1_digits.sh
+seeds=( 888 1 2410 1996 511 )
+scales=( 0.0 )
+
+slots=1
+
+for scale in "${scales[@]}"; do
+    echo $scale
+    for seed in "${seeds[@]}"; do
+        echo $seed
+        qsub -pe smpslots $slots python-ot.job run_both.py @flags/pipeline_cmnist_2.yaml \
+        --b-super-val-freq 20 \
+        --b-super-val True \
+        --b-gpu 0 \
+        --c-use-wandb False \
+        --c-method kmeans \
+        --b-seed $seed \
+        --b-data-split-seed $seed \
+        --b-scale $scale \
+        --d-results cmnist_2digits_$seed\_$scale.csv \
+        --b-save-dir experiments/cmnist/2digits/$seed/$scale $@
+        sleep 1
+    done
+done
