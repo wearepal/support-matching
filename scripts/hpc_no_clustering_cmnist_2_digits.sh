172459d6cf3dab63fff16724209fce438c3c4cc9
Oliver Thomas <1835492+olliethomas@users.noreply.github.com>
2020-06-03 18:18:00 +0100

    script to run baseline 1 on fear

--- scripts/hpc_no_clustering_cmnist_2_digits.sh
+++ scripts/hpc_no_clustering_cmnist_2_digits.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #Expects cwd to be fair-dist-matching. i.e ./scripts/cmnist_1_digits.sh
 seeds=( 888 1 2410 1996 511 )
-scales=( 0.0 )
+scales=( 0.0 0.005 0.01 0.015 0.02 0.025 0.03 0.035 0.04 0.045 0.05 )
 
 slots=1
 
@@ -9,7 +9,7 @@ for scale in "${scales[@]}"; do
     echo $scale
     for seed in "${seeds[@]}"; do
         echo $seed
-        python run_no_balancing.py @flags/mvp_args.yaml \
+        qsub -pe smpslots $slots python-ot.job run_no_balancing.py @flags/mvp_args.yaml \
         --b-super-val-freq 20 \
         --b-super-val True \
         --b-gpu 0 \
