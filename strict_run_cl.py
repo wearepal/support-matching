715454e9d6f15b9ffd8e3255eed8ede0c2f5605b
Thomas M <thomas5max@gmail.com>
2020-06-04 14:57:56 +0100

    Add run scripts that are strict about flag names

---
+++ strict_run_cl.py
@@ -0,0 +1,8 @@
+"""Simply call the main function"""
+import sys
+
+assert sys.version_info >= (3, 8), f"please use Python 3.8 (this is 3.{sys.version_info.minor})"
+from clustering.optimisation import main
+
+if __name__ == "__main__":
+    main(known_only=False)
