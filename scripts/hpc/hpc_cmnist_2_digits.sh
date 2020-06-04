877f1efe68ec9d0a2718bb4830a99602effea934
Oliver Thomas <1835492+olliethomas@users.noreply.github.com>
2020-06-04 13:47:23 +0100

    change flags

--- scripts/hpc/hpc_cmnist_2_digits.sh
+++ scripts/hpc/hpc_cmnist_2_digits.sh
@@ -1,24 +1,21 @@
 #!/usr/bin/env bash
 #Expects cwd to be fair-dist-matching. i.e ./scripts/cmnist_1_digits.sh
-seeds=( 888 1 2410 1996 511 )
-scales=( 0.0 )
+seeds=( 2512 711 142 )
 
-slots=1
+slots=6
 
-for scale in "${scales[@]}"; do
-    echo $scale
-    for seed in "${seeds[@]}"; do
-        echo $seed
-        qsub -pe smpslots $slots python-ot.job run_both.py @flags/mvp_args.yaml \
-        --b-super-val-freq 20 \
-        --b-super-val True \
-        --b-gpu 0 \
-        --c-use-wandb False \
-        --b-seed $seed \
-        --b-data-split-seed $seed \
-        --b-scale $scale \
-        --d-results cmnist_2digits_$seed\_$scale.csv \
-        --b-save-dir experiments/cmnist/ours/full/2digits/$seed/$scale $@
-        sleep 1
-    done
+echo $scale
+for seed in "${seeds[@]}"; do
+    echo $seed
+    qsub -pe smpslots $slots python-ot.job run_both.py @flags/mvp_args.yaml \
+    --b-super-val-freq 20 \
+    --b-super-val True \
+    --b-gpu 0 \
+    --c-use-wandb False \
+    --b-seed $seed \
+    --b-data-split-seed $seed \
+    --b-scale 0.0 \
+    --d-results cmnist_2digits_$seed\_0.0.csv \
+    --b-save-dir experiments/cmnistmissings/ours/full/2digits/$seed/0.0 $@
+    sleep 1
 done
