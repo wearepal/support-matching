41e1f96b7ce964c3f66533ddaa53c3723663aa47
Oliver Thomas <1835492+olliethomas@users.noreply.github.com>
2020-06-04 15:51:02 +0100

    seeds

--- scripts/cmnist_kmeans_2_digits.sh
+++ scripts/cmnist_kmeans_2_digits.sh
@@ -1,25 +1,19 @@
 #!/usr/bin/env bash
 #Expects cwd to be fair-dist-matching. i.e ./scripts/cmnist_1_digits.sh
-seeds=( 888 1 2410 1996 511 )
-scales=( 0.0 )
+#seeds=( 888 1 2410 1996 511 )
+seeds=( 711 142 )
 
-slots=1
-
-for scale in "${scales[@]}"; do
-    echo $scale
-    for seed in "${seeds[@]}"; do
-        echo $seed
-        python run_both.py @flags/mvp_args.yaml \
-        --b-super-val-freq 20 \
-        --b-super-val True \
-        --b-gpu 0 \
-        --c-use-wandb False \
-        --c-method kmeans \
-        --b-seed $seed \
-        --b-data-split-seed $seed \
-        --b-scale $scale \
-        --d-results cmnist_2digits_$seed\_$scale.csv \
-        --b-save-dir experiments/cmnist/ours/kmeans/2digits/$seed/$scale $@
-        sleep 1
-    done
+for seed in "${seeds[@]}"; do
+    echo $seed
+    python run_both.py @flags/mvp_args.yaml \
+    --b-super-val-freq 20 \
+    --b-super-val True \
+    --b-gpu 0 \
+    --c-method kmeans \
+    --b-seed $seed \
+    --b-data-split-seed $seed \
+    --b-scale 0.0 \
+    --d-results cmnist_2digits_$seed\_0.0.csv \
+    --b-save-dir experiments/cmnist/ours/kmeans/2digits/$seed/0.0 $@
+    sleep 1
 done
--- scripts/no_clustering_cmnist_2_digits.sh
+++ scripts/no_clustering_cmnist_2_digits.sh
@@ -1,23 +1,19 @@
 #!/usr/bin/env bash
 #Expects cwd to be fair-dist-matching. i.e ./scripts/cmnist_1_digits.sh
-seeds=( 888 1 2410 1996 511 )
-scales=( 0.0 )
+#seeds=( 888 1 2410 1996 511 )
+seeds=( 711 142 )
 
-slots=1
 
-for scale in "${scales[@]}"; do
-    echo $scale
-    for seed in "${seeds[@]}"; do
-        echo $seed
-        python run_no_balancing.py @flags/mvp_args.yaml \
-        --b-super-val-freq 20 \
-        --b-super-val True \
-        --b-gpu 0 \
-        --b-seed $seed \
-        --b-data-split-seed $seed \
-        --b-scale $scale \
-        --d-results nocluster_cmnist_2digits_$seed\_$scale.csv \
-        --b-save-dir experiments/cmnist/baseline/nocluster/2digits/$seed/$scale $@
-        sleep 1
-    done
+for seed in "${seeds[@]}"; do
+    echo $seed
+    python run_no_balancing.py @flags/mvp_args.yaml \
+    --b-super-val-freq 20 \
+    --b-super-val True \
+    --b-gpu 0 \
+    --b-seed $seed \
+    --b-data-split-seed $seed \
+    --b-scale 0.0 \
+    --d-results nocluster_cmnist_2digits_$seed\_0.0.csv \
+    --b-save-dir experiments/cmnist/baseline/nocluster/2digits/$seed/0.0 $@
+    sleep 1
 done
