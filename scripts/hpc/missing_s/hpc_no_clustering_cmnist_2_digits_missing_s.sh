17000a4ac8084f49fa4c99cb6a5e49a816b8b36c
Oliver Thomas <1835492+olliethomas@users.noreply.github.com>
2020-06-05 01:52:54 +0100

    expand filename

--- scripts/hpc/missing_s/hpc_no_clustering_cmnist_2_digits_missing_s.sh
+++ scripts/hpc/missing_s/hpc_no_clustering_cmnist_2_digits_missing_s.sh
@@ -11,7 +11,7 @@ for seed in "${seeds[@]}"; do
     --b-gpu 0 \
     --b-seed $seed \
     --b-data-split-seed $seed \
-    --d-results cmnist_2digits_$seed.csv \
+    --d-results cmnist_missing_s0_no_cluster_2digits_$seed.csv \
     --b-save-dir experiments/cmnistmissings/ours/full/2digits/$seed $@
     sleep 1
 done
