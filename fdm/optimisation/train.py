1de59985a333406b8b375ea5697ca02006a03234
Myles Bartlett <44702700+MylesBartlett@users.noreply.github.com>
2020-08-09 12:25:27 +0100

    Make snorm toggleable.

--- fdm/optimisation/train.py
+++ fdm/optimisation/train.py
@@ -262,10 +262,8 @@ def main(
             encoding_size=encoding_size,
             feature_group_slices=feature_group_slices,
         )
-        # if ARGS.vae:
-        # generator.apply(_spectral_norm)
-        # else:
-        #     generator.apply(_weight_norm)
+        if ARGS.snorm:
+            generator.apply(_spectral_norm)
 
     LOGGER.info("Encoding shape: {}, {}", enc_shape, encoding_size)
 
