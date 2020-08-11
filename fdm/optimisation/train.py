7c8875770c181d0e99b34eb696770259c9207339
Myles Bartlett <44702700+MylesBartlett@users.noreply.github.com>
2020-08-11 12:50:14 +0100

    .

--- fdm/optimisation/train.py
+++ fdm/optimisation/train.py
@@ -528,8 +528,6 @@ def update_disc(x_c: Tensor, x_t: Tensor, ae: AeComponents, warmup: bool = False
             disc_input_c = disc_input_c.detach()
 
         # discriminator is trained to distinguish `disc_input_c` and `disc_input_t`
-        # disc_loss_true = ae.discriminator(disc_input_c).mean()
-        # disc_loss_false = ae.discriminator(disc_input_t).mean()
         disc_loss_true, _ = ae.discriminator.routine(disc_input_c, ones)
         disc_loss_false, _ = ae.discriminator.routine(disc_input_t, zeros)
         disc_loss += disc_loss_true + disc_loss_false
@@ -590,7 +588,6 @@ def update(
     # ==================================== adversarial losses =====================================
     disc_input_no_s = get_disc_input(ae.generator, encoding, invariant_to="s")
     zeros = x_t.new_zeros((x_t.size(0),))
-    # disc_loss = ae.discriminator(disc_input_no_s).mean()
     disc_loss, _ = ae.discriminator.routine(disc_input_no_s, zeros)
 
     pred_y_loss = x_t.new_zeros(())
