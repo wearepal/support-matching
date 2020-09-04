226e3e21a54d19409931f22933c8fc731424c7b7
Myles Bartlett <m.bartlett@sussex.ac.uk>
2020-09-04 14:45:32 +0100

    Squeeze class_id before returning.

--- clustering/optimisation/utils.py
+++ clustering/optimisation/utils.py
@@ -121,7 +121,7 @@ def get_class_id(
         class_id = y
     else:
         class_id = y * s_count + s
-    return class_id
+    return class_id.squeeze()
 
 
 def get_cluster_label_path(args: ClusterArgs, save_dir: Path) -> Path:
