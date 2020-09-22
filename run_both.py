b803bba13ab31f368de566d137bc9b4139e70dac
Thomas M <thomas5max@gmail.com>
2020-09-22 19:32:43 +0100

    Hot fix

--- run_both.py
+++ run_both.py
@@ -13,8 +13,8 @@ def main() -> None:
         clf = str(Path(tmpdir) / "labels.pth")
         clf_flag = ["--cluster-label-file", clf]
         try:
-            run([sys.executable, "run_cl.py"] + raw_args + clf_flag, check=True)
-            run([sys.executable, "run_d.py"] + raw_args + clf_flag, check=True)
+            run([sys.executable, "run_clust.py"] + raw_args + clf_flag, check=True)
+            run([sys.executable, "run_dis.py"] + raw_args + clf_flag, check=True)
         except CalledProcessError as cpe:
             # catching the exception ourselves leads to much nicer error messages
             print(f"\nCommand '{shlex.join(cpe.cmd)}'")
