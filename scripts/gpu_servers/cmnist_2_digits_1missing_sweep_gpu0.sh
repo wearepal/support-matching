f572ae87827072bce5438ae0d51ca7340d42e0f3
Thomas M <thomas5max@gmail.com>
2020-09-24 20:21:08 +0100

    Add save_dir

--- scripts/gpu_servers/cmnist_2_digits_1missing_sweep_gpu0.sh
+++ scripts/gpu_servers/cmnist_2_digits_1missing_sweep_gpu0.sh
@@ -9,6 +9,7 @@ MAX_SEED=10
 seeds=$(seq 1 $MAX_SEED)
 flag_file=flags/sleek_sky.yaml
 shared_flags="--missing-s"
+save_dir="experiments/cmnist/2digits/2colors/1missing/no_subsample"
 
 # get the experiment loops for the methods
 source scripts/gpu_servers/method_loops.sh
