83ab02dfc86bb9200e56a80559dc8ba74b7c60cc
Thomas M <thomas5max@gmail.com>
2020-10-01 15:47:21 +0000

    Sleep between job submissions

--- scripts/gpu_servers/run_all.sh
+++ scripts/gpu_servers/run_all.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 nohup bash scripts/gpu_servers/ultimate_cmnist2digits.sh 1 &
+sleep 10
 nohup bash scripts/gpu_servers/ultimate_cmnist2digits.sh 2 &
+sleep 10
 nohup bash scripts/gpu_servers/ultimate_cmnist2digits.sh 3 &
+sleep 10
 nohup bash scripts/gpu_servers/ultimate_cmnist2digits.sh 4 &
+sleep 10
 nohup bash scripts/gpu_servers/ultimate_cmnist2digits.sh 5 &
+sleep 10
 nohup bash scripts/gpu_servers/ultimate_cmnist2digits.sh 6 &
+sleep 10
 nohup bash scripts/gpu_servers/ultimate_cmnist2digits.sh 7 &
+sleep 10
 nohup bash scripts/gpu_servers/ultimate_cmnist2digits.sh 8 &
