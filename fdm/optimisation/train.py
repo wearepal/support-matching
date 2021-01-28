Correct sign of disc_loss.

fdm/optimisation/train.py
@@ -7,17 +7,8 @@ from pathlib import Path
 import time
 from typing import NamedTuple, cast
 
-import git
-from hydra.utils import to_absolute_path
 import numpy as np
-import torch
-from torch import Tensor
-from torch.cuda.amp.grad_scaler import GradScaler
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.utils.data import DataLoader, Dataset
 from typing_extensions import Literal
-import wandb
 import yaml
 
 from fdm.models import AutoEncoder, Classifier, EncodingSize, build_classifier
@@ -25,6 +16,8 @@ from fdm.models.configs import Residual64x64Net
 from fdm.models.configs.classifiers import Strided28x28Net
 from fdm.models.discriminator import Discriminator
 from fdm.optimisation.mmd import mmd2
+import git
+from hydra.utils import to_absolute_path
 from shared.configs import (
     AggregatorType,
     Config,
@@ -62,6 +55,13 @@ from shared.utils import (
     readable_duration,
     wandb_log,
 )
+import torch
+from torch import Tensor
+from torch.cuda.amp.grad_scaler import GradScaler
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader, Dataset
+import wandb
 
 from .build import build_ae
 from .evaluation import baseline_metrics, log_metrics
@@ -262,7 +262,7 @@ class Experiment(ExperimentBase):
                         add_dot=self.args.mmd_add_dot,
                     )
                 disc_loss *= self.args.disc_weight
-                total_loss -= disc_loss  # subtract the loss!!
+                total_loss += disc_loss
                 logging_dict["Loss Discriminator"] = disc_loss
 
             # this is a pretty cheap masking operation, so it's okay if it's not used