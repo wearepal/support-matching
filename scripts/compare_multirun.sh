978340c793f8511d21b7b264f7ac48dc7b06195c
Thomas M <thomas5max@gmail.com>
2021-02-08 18:46:11 +0000

    Invert the order of runs in compare*.sh

--- scripts/compare_multirun.sh
+++ scripts/compare_multirun.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+# perfect cluster
+python run_dis.py -m fdm.balanced_context=true misc.log_method=perfect-cluster "$@"
+
+# no cluster
+python run_dis.py -m fdm.balanced_context=false misc.log_method=no-cluster-fdm "$@"
+
 # ranking
 python run_pipeline.py -m clust.method=pl_enc_no_norm clust.pseudo_labeler=ranking misc.log_method=ranking-fdm "$@"
 
 # kmeans
 python run_pipeline.py -m clust.method=kmeans misc.log_method=kmeans-fdm "$@"
-
-# no cluster
-python run_dis.py -m fdm.balanced_context=false misc.log_method=no-cluster-fdm "$@"
-
-# perfect cluster
-python run_dis.py -m fdm.balanced_context=true misc.log_method=perfect-cluster "$@"
