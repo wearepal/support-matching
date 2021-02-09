7bfcff71096813c8973071f77687e905732b8d1f
Thomas M <thomas5max@gmail.com>
2021-02-09 15:29:49 +0000

    Don't use double_adv_loss anymore

--- adult.sh
+++ adult.sh
@@ -9,15 +9,15 @@ python run_dis.py -m \
     fdm=adult/simple_on_enc \
     fdm.eval_epochs=60 \
     fdm.balanced_eval=True \
-    fdm.iters=4000 \
+    fdm.iters=6000 \
     fdm.validate=False \
-    fdm.aggregator_type=none,gated,kvq \
+    fdm.aggregator_type=gated,kvq \
     fdm.num_disc_updates=1,3 \
     fdm.pred_s_weight=1.0,0.0 \
     fdm.zs_dim=1 \
     fdm.zs_transform=none,round_ste \
-    fdm.double_adv_loss=True,False \
-    misc.exp_group='agg-${fdm.aggregator_type}.upd-${fdm.num_disc_updates}.preds-${fdm.pred_s_weight}.zst-${fdm.zs_transform}.dal-${fdm.double_adv_loss}' \
+    fdm.double_adv_loss=False \
+    misc.exp_group='agg-${fdm.aggregator_type}.upd-${fdm.num_disc_updates}.preds-${fdm.pred_s_weight}.zst-${fdm.zs_transform}.bs-${fdm.batch_size}.bg-${fdm.bag_size}' \
     misc.seed="range(0,10)" \
     hydra/launcher=ray_0.33gpus \
     "$@"
