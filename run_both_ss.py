5a087a4d0378d4ced721a4095e7fd141e5158a8e
Myles Bartlett <m.bartlett@sussex.ac.uk>
2021-05-22 23:38:25 +0100

    Reverse order of raw_args and clf_flag in run_both scripts for multirun compatibility.

--- run_both_ss.py
+++ run_both_ss.py
@@ -12,8 +12,8 @@ def main() -> None:
         clf = str(Path(tmpdir) / "labels.pth")
         clf_flag = [f"misc.cluster_label_file={clf}"]
         try:
-            run([sys.executable, "run_clust.py"] + raw_args + clf_flag, check=True)
-            run([sys.executable, "run_ss.py"] + raw_args + clf_flag, check=True)
+            run([sys.executable, "run_clust.py"] + clf_flag + raw_args, check=True)
+            run([sys.executable, "run_ss.py"] + clf_flag + raw_args, check=True)
         except CalledProcessError as cpe:
             # catching the exception ourselves leads to much nicer error messages
             print(f"\nCommand '{' '.join(cpe.cmd)}'")
