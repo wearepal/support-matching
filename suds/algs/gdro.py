07c8c25805db78941b517c70edc8d7d799c6a20a
Myles Bartlett <m.bartlett@sussex.ac.uk>
2021-05-24 19:59:37 +0100

    Remove stats again.

--- suds/algs/gdro.py
+++ suds/algs/gdro.py
@@ -80,9 +80,6 @@ class LossComputer(nn.Module):
             actual_loss = per_sample_losses.mean()
             weights = None
 
-        # update stats
-        self.update_stats(actual_loss, group_loss, group_acc, group_count, weights)
-
         return actual_loss
 
     def compute_robust_loss(self, group_loss: Tensor, group_count: Tensor) -> tuple[Tensor, Tensor]:
@@ -140,79 +137,6 @@ class LossComputer(nn.Module):
         curr_weights = 1 - prev_weights
         self.exp_avg_loss = self.exp_avg_loss * prev_weights + group_loss * curr_weights
         self.exp_avg_initialized = (self.exp_avg_initialized > 0) + (group_count > 0)
-
-    def reset_stats(self) -> None:
-        device = cast(torch.device, self.exp_avg_loss.device)
-        self.processed_data_counts = torch.zeros(self.n_groups, device=device)
-        self.update_data_counts = torch.zeros(self.n_groups, device=device)
-        self.update_batch_counts = torch.zeros(self.n_groups, device=device)
-        self.avg_group_loss = torch.zeros(self.n_groups, device=device)
-        self.avg_group_acc = torch.zeros(self.n_groups, device=device)
-        self.avg_per_sample_loss = 0.0
-        self.avg_actual_loss = 0.0
-        self.avg_acc = 0.0
-        self.batch_count = 0.0
-
-    def update_stats(self, actual_loss, group_loss, group_acc, group_count, weights=None):
-        # avg group loss
-        denom = self.processed_data_counts + group_count
-        denom += (denom == 0).float()
-        prev_weight = self.processed_data_counts / denom
-        curr_weight = group_count / denom
-        self.avg_group_loss = prev_weight * self.avg_group_loss + curr_weight * group_loss
-
-        # avg group acc
-        self.avg_group_acc = prev_weight * self.avg_group_acc + curr_weight * group_acc
-
-        # batch-wise average actual loss
-        denom = self.batch_count + 1
-        self.avg_actual_loss = (self.batch_count / denom) * self.avg_actual_loss + (
-            1 / denom
-        ) * actual_loss
-
-        # counts
-        self.processed_data_counts += group_count
-        if self.is_robust:
-            self.update_data_counts += group_count * ((weights > 0).float())
-            self.update_batch_counts += ((group_count * weights) > 0).float()
-        else:
-            self.update_data_counts += group_count
-            self.update_batch_counts += (group_count > 0).float()
-        self.batch_count += 1
-
-        # avg per-sample quantities
-        group_frac = self.processed_data_counts / (self.processed_data_counts.sum())
-        self.avg_per_sample_loss = group_frac @ self.avg_group_loss
-        self.avg_acc = group_frac @ self.avg_group_acc
-
-    def get_model_stats(self, model, args, stats_dict):
-        model_norm_sq = 0.0
-        for param in model.parameters():
-            model_norm_sq += torch.norm(param) ** 2
-        stats_dict["model_norm_sq"] = model_norm_sq.item()
-        stats_dict["reg_loss"] = args.weight_decay / 2 * model_norm_sq.item()
-        return stats_dict
-
-    def get_stats(self, model=None, args=None):
-        stats_dict = {}
-        for idx in range(self.n_groups):
-            stats_dict[f"avg_loss_group:{idx}"] = self.avg_group_loss[idx].item()
-            stats_dict[f"exp_avg_loss_group:{idx}"] = self.exp_avg_loss[idx].item()
-            stats_dict[f"avg_acc_group:{idx}"] = self.avg_group_acc[idx].item()
-            stats_dict[f"processed_data_count_group:{idx}"] = self.processed_data_counts[idx].item()
-            stats_dict[f"update_data_count_group:{idx}"] = self.update_data_counts[idx].item()
-            stats_dict[f"update_batch_count_group:{idx}"] = self.update_batch_counts[idx].item()
-
-        stats_dict["avg_actual_loss"] = self.avg_actual_loss.item()
-        stats_dict["avg_per_sample_loss"] = self.avg_per_sample_loss.item()
-        stats_dict["avg_acc"] = self.avg_acc.item()
-
-        # Model stats
-        if model is not None:
-            assert args is not None
-            stats_dict = self.get_model_stats(model, args, stats_dict)
-
-        return stats_dict
 
 
 class GDRO(Classifier):
@@ -272,7 +196,7 @@ class GDRO(Classifier):
                     pin_memory=train_data.pin_memory,
                     num_workers=train_data.num_workers,
                 )
-        self.loss_computer.reset_stats()
+
         LOGGER.info("Training classifier...")
         pbar = trange(epochs)
         for epoch in pbar:
