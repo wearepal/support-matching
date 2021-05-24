1baf9cafd997a96fac4a71581593f2927e64b771
Thomas M <thomas5max@gmail.com>
2021-05-24 14:57:45 +0100

    Add another test to calm my conscience

--- tests/joint_prob_test.py
+++ tests/joint_prob_test.py
@@ -28,3 +28,14 @@ def _compare(s: Tensor, y: Tensor, s_count: int, y_count: int) -> None:
     joint = get_joint_probability(s_probs=s_probs, y_probs=y_probs)
 
     assert_allclose(class_ids, joint.argmax(dim=-1), atol=0, rtol=0)
+
+
+def test_compatibility_reversed() -> None:
+    s_probs = torch.tensor([[0.3, 0.7], [0.2, 0.8], [0.9, 0.1], [0.4, 0.5]])
+    y_probs = torch.tensor([[0.4, 0.5], [0.9, 0.1], [0.2, 0.8], [0.3, 0.7]])
+
+    joint = get_joint_probability(s_probs=s_probs, y_probs=y_probs)
+
+    class_ids = label_to_class_id(s=s_probs.argmax(dim=-1), y=y_probs.argmax(dim=-1), s_count=2)
+
+    assert_allclose(class_ids, joint.argmax(dim=-1), atol=0, rtol=0)
