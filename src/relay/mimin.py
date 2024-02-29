14a2978b628e19b2f3c4c127bd3b64eda00e8540
Thomas M Kehrenberg <tmke8@posteo.net>
2024-02-29 17:08:34 +0100

    Update mimin as well

--- src/relay/mimin.py
+++ src/relay/mimin.py
@@ -72,7 +72,13 @@ class MiMinRelay(BaseRelay):
         ds = self.ds()
         run = self.wandb.init(raw_config, (ds, self.labeller, self.ae_arch, self.disc_arch))
         dm = self.init_dm(ds, self.labeller, device=self.alg.device)
-        input_shape: tuple[int, int, int] = dm.dim_x  # type: ignore
+        match dm.dim_x:
+            case (c,):
+                input_shape = (c, 0, 0)
+            case (c, h, w):
+                input_shape = (c, h, w)
+            case _:
+                raise ValueError(f"Unsupported input shape: {dm.dim_x}")
         ae_pair = self.ae_arch(input_shape=input_shape)
         ae = SplitLatentAe(opt=self.ae, model=ae_pair, feature_group_slices=dm.feature_group_slices)
         logger.info(f"Encoding dim: {ae.latent_dim}, {ae.encoding_size}")
